621057879e46badb3a4effd7d317dd8f8fb83819
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-14 17:09:18 -0500

    Pruebas con @Input().
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -2,6 +2,8 @@ import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Person } from 'src/app/models/person.model';
+
 import { PersonComponent } from './person.component';
 
 describe('PersonComponent', () => {
@@ -13,9 +15,12 @@ describe('PersonComponent', () => {
       declarations: [PersonComponent]
     })
       .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    component.person = new Person('Fulano', 'Detal', 25, 82, 1.77);
     fixture.detectChanges();
   });
 
@@ -23,17 +28,34 @@ describe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Shoul have a <h3> tag with the text "Hola, Bienvenido"', () => {
-    const el: HTMLElement = fixture.nativeElement;
-    const h3 = el.querySelector('h3');
-    expect(h3?.textContent).toEqual("Hola, Bienvenido");
+  it('should have the name "Fulano"', () => {
+    expect(component.person?.name).toEqual('Fulano');
   });
 
-  it('Shoul have a <p> tag with a paragraph with more than 10 characters', () => {
+  it('should have a the name "Fulanito" in the <p> tag', () => {
+    // Arrange
+    component.person = new Person('Fulanito', 'Detal', 25, 82, 1.77);
     const debugEl: DebugElement = fixture.debugElement;
-    const hDebug = debugEl.query(By.css('p'));
-    const p: HTMLElement = hDebug.nativeElement;
+    const hDebug = debugEl.query(By.css('h3'));
+    const h3: HTMLElement = hDebug.nativeElement;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(h3?.textContent).toContain("Fulanito");
+  });
+
+  it('should have a <h3> tag with the text "Hola, Fulano"', () => {
+    const el: HTMLElement = fixture.nativeElement;
+    const h3 = el.querySelector('h3');
+    expect(h3?.textContent).toEqual("Hola, Fulano");
+  });
+
+  it('should have a <p> tag with a paragraph with more than 10 characters', () => {
+    const debugEl: DebugElement = fixture.debugElement;
+    const pDebug = debugEl.query(By.css('p'));
+    const p: HTMLElement = pDebug.nativeElement;
     // const p: HTMLElement = debugEl.nativeElement;
     expect(p?.textContent?.length).toBeGreaterThanOrEqual(10);
+    expect(p?.textContent).toContain(1.77);
   });
 });
