cd4d5b2a4c53ece28a5eebf5fb42d8f08f14382f
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-17 14:51:34 -0500

    Pruebas desde el componente padre.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -8,11 +8,20 @@ import { Person } from 'src/app/models/person.model';
 })
 export class PeopleComponent implements OnInit {
 
-  person = new Person('Fulano', 'Detal', 25, 82, 1.77);
+  selectedPerson: Person | undefined;
+
+  people: Person[] = [
+    new Person('Alan', 'Brito', 21, 75, 1.74),
+    new Person('Fulano', 'Detal', 25, 82, 1.77),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setPerson(p: Person) {
+    this.selectedPerson = p;
   }
 
 }
